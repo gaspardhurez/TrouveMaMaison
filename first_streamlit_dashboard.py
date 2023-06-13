80e6be5055677a531e9aae8dc0b5af1737b2fb5f
gaspardhurez <hurezgaspard@gmail.com>
2023-06-13 22:53:14 +0200

    Update first_streamlit_dashboard.py

--- first_streamlit_dashboard.py
+++ first_streamlit_dashboard.py
@@ -99,12 +99,12 @@ fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
 with st.container():
 
-    st.markdown('<div class="horizontal-align-container">', unsafe_allow_html=True)
-    st.markdown('<div class="centered-table">', unsafe_allow_html=True)
+    st.write('<div class="horizontal-align-container">', unsafe_allow_html=True)
+    st.write('<div class="centered-table">', unsafe_allow_html=True)
     st.table(df[['department_name', 'global_score']].nlargest(10, 'global_score'))
     st.plotly_chart(fig, use_container_width=True)
-    st.markdown('</div>', unsafe_allow_html=True)
-    st.markdown('</div>', unsafe_allow_html=True)
+    st.write('</div>', unsafe_allow_html=True)
+    st.write('</div>', unsafe_allow_html=True)
 
 
 
