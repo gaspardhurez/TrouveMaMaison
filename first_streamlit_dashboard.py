4107f813432f9653097b4b5206511162ef4c2624
gaspardhurez <hurezgaspard@gmail.com>
2023-06-13 21:51:30 +0200

    Update first_streamlit_dashboard.py

--- first_streamlit_dashboard.py
+++ first_streamlit_dashboard.py
@@ -22,7 +22,7 @@ st.sidebar.header('Preferences')
 housing_score = st.sidebar.slider('Profitability', 0, 5, value=5)
 climate_score = st.sidebar.slider('Climate', 0, 5, value=5)
 tourism_score = st.sidebar.slider('Tourism', 0, 5, value=5)
-dev_score = st.siderbar.slider('Development', 0, 5, value=5)
+dev_score = st.sidebar.slider('Development', 0, 5, value=5)
 total_score = housing_score + climate_score + tourism_score + dev_score
 
 if total_score != 0:
