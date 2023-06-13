778786afbf97b6c1e1ea4088672b351800003494
gaspardhurez <hurezgaspard@gmail.com>
2023-06-13 22:35:44 +0200

    Update first_streamlit_dashboard.py

--- first_streamlit_dashboard.py
+++ first_streamlit_dashboard.py
@@ -55,45 +55,46 @@ line_data = st.sidebar.multiselect('Select data', ['global_score', 'tourism_scor
 # st.title('Where should I buy my secondary home?')
 # st.markdown("---")
 
+
+col1, col2 = st.columns((4, 3), gap='large')
+
+col1.markdown('### Top 10 departments')
+
+query = 'SELECT * FROM `scoring_tables.scores`'
+df = pd.read_gbq(query, project_id="team-prello-jogaan", credentials=credentials)
+df['tourism_score'] = df['tourism_score'].astype(float)
+df['climate_score'] = df['climate_score'].astype(float)
+df['immo_score'] = df['immo_score'].astype(float)
+df['dev_score'] = df['dev_score'].astype(float)
+df['global_score'] = round((df['tourism_score'] * tourism_weight
+                            + df['climate_score'] * climate_weight
+                            + df['immo_score'] * housing_weight
+                            + df['dev_score'] * dev_weight), 2)
+
+df_geo = pd.read_csv('data/geo_departments.csv')
+df = pd.merge(df, df_geo, on='department_name')
+
+geodata = gpd.read_file('data/departements.geojson')
+df_geo = pd.merge(geodata, df, left_on='nom', right_on='department_name', how='inner')
+
+top_10_departments = df_geo.nlargest(10, 'global_score')
+fig = px.choropleth_mapbox(
+    top_10_departments,
+    geojson=top_10_departments.geometry,
+    locations=top_10_departments.index,
+    color='global_score',
+    mapbox_style="carto-positron",
+    center={"lat": 46.8, "lon": 1.8},
+    zoom=4.5,
+    opacity=0.7,
+    labels={'global_score': 'Global Score'},
+)
+
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
 # First line : map and top 10
 
 with st.container():
-
-    col1, col2 = st.columns((4, 3), gap='large')
-
-    col1.markdown('### Top 10 departments')
-
-    query = 'SELECT * FROM `scoring_tables.scores`'
-    df = pd.read_gbq(query, project_id="team-prello-jogaan", credentials=credentials)
-    df['tourism_score'] = df['tourism_score'].astype(float)
-    df['climate_score'] = df['climate_score'].astype(float)
-    df['immo_score'] = df['immo_score'].astype(float)
-    df['dev_score'] = df['dev_score'].astype(float)
-    df['global_score'] = round((df['tourism_score'] * tourism_weight
-                                + df['climate_score'] * climate_weight
-                                + df['immo_score'] * housing_weight
-                                + df['dev_score'] * dev_weight), 2)
-
-    df_geo = pd.read_csv('data/geo_departments.csv')
-    df = pd.merge(df, df_geo, on='department_name')
-
-    geodata = gpd.read_file('data/departements.geojson')
-    df_geo = pd.merge(geodata, df, left_on='nom', right_on='department_name', how='inner')
-
-    top_10_departments = df_geo.nlargest(10, 'global_score')
-    fig = px.choropleth_mapbox(
-        top_10_departments,
-        geojson=top_10_departments.geometry,
-        locations=top_10_departments.index,
-        color='global_score',
-        mapbox_style="carto-positron",
-        center={"lat": 46.8, "lon": 1.8},
-        zoom=4.5,
-        opacity=0.7,
-        labels={'global_score': 'Global Score'},
-    )
-
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
     st.markdown('<div class="horizontal-align-container">', unsafe_allow_html=True)
     col1.plotly_chart(fig, use_container_width=True)
